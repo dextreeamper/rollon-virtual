4bbe7575203194b1eb3efda8572bf36463c2c6db
Dextree Amper <appsdex92@gmail.com>
2017-08-25 21:42:47 +0800

    Adding gulp sass and gitignore file

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+	return gulp.src('./css/style.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('./css/style.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'watch']);
+
